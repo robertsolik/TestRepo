e745fd662095e0828f0c07160fe68161639601cb
robertsolik <robert.solik@gmail.com>
2018-07-14 12:57:15 -0700

    age calc

--- test01.py
+++ test01.py
@@ -1,7 +1,13 @@
 # Test01
-
+import datetime
 #print("Hello World")
-list = ['hello','world','my','name','is','Robert']
+#list = ['hello','world','my','name','is','Robert']
 
-for word in list:
-    print(word.title())
+# for word in list:
+#     print(word.title())
+dob_input = input('Enter your date of birth (yyyy-mm-dd): ')
+year, month, day = map(int, dob_input.split('-'))
+dob = datetime.date(year, month, day)
+today = datetime.date.today()
+age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+print(f"You are {age} years old.")
